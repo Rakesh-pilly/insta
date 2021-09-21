e97afe64d95b5cfe087bfc33ae6a860bccb1aeac
Rakesh-pilly <rakesh.mudhiraj882@gmail.com>
2021-09-21 12:39:47 +0530

    some changes in opportuities

--- src/components/Opportunities/OpportunityViews.jsx
+++ src/components/Opportunities/OpportunityViews.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import styles from "../Opportunities/Opportunity.module.css"
+import styles from "../Opportunities/Opportunity.module.css";
+import {useState} from "react";
+import axios from "axios";
 
 
-const OpportunityViews = ({jobs}) => {
-    // const [jobs, setJobs] = useState('');
+const OpportunityViews = () => {
+    const [jobs, setJobs] = useState('');
 
-    // if (!jobs) {
-    //     axios.get('http://localhost:3001/jobs').then(res => { console.log(res, 'res'); setJobs(res.data) })
-    // }
+    if (!jobs) {
+        axios.get('http://localhost:3000/jobs').then(res => { console.log(res, 'res'); setJobs(res.data) })
+    }
 
     if (jobs) {
        
